464d85fb8612e3419dded838f03dd0d4184c7f0a
acyrwus <acyrwus@asu.edu>
2019-10-05 21:43:36 -0700

    Initial Commit

---
+++ Assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,56 @@
+package cse360assign2;
+
+/*
+ * @author: Alex Cyrwus
+ * 
+ * This class functions as a simple calculator
+ * It also has the ability to keep track of the commands it has used.
+ */
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/*
+	 * Getter for total
+	 * @return: Total value
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	 * Adds the value to the total
+	 * @param: The value you want to get added to the total
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	/*
+	 * Subtracts the value from the Total
+	 * @param: The value you want to be subtracted from Total
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/*
+	 * Gives a log of all calculations done in the class
+	 * @return: A string containing the calculations done in the class
+	 */
+	
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
